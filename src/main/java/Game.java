623e5176cdc70dcf87113214f7fd04832764cc02
Trifonov Dmitriy <62952967+Djivs@users.noreply.github.com>
2021-10-23 21:01:08 +0300

    Delete unnecessary libs

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,15 +1,7 @@
 package main.java;
 
 import lib.Window;
-import lib.render.Canvas;
 import lib.render.Texture;
-import lib.vertex.DefaultVertexFormats;
-import org.lwjgl.glfw.GLFW;
-import org.lwjgl.opengl.GL11;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class Game extends Window {
     private final Texture backgroundTexture = Texture.load("back.jpg");
